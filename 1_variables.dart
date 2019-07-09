fc1a204d1dc25e86a5ff81f9e324c60cc22a507e
lintangwisesa <lintangwisesa@ymail.com>
2019-07-09 19:57:19 +0700

    dart fundamentals

--- 1_variables.dart
+++ 1_variables.dart
@@ -3,9 +3,14 @@ void main(){
   int b = 12;
   double c = 3.14;
   bool d = true;
+  var e = 99;
 
   print(a);
   print(b);
   print(c);
   print(d);
+  print(e);
+
+  String nama = "Alibaba";
+  print('Hi, my name is ${nama}');
 }
